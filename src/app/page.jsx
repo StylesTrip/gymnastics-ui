66ce541c868b6d0df681963a236ba2a9470878c8
StylesTrip <ryan.turinsky@gmail.com>
2023-03-24 01:15:39 -0400

    Making updates to the home page to add more detail

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 import './globals.css';
 import bgPicture from '../../public/emma2.jpeg';
+import Image from 'next/image';
+import Link from 'next/Link';
 
 export default function Home() {
     return (
@@ -15,6 +17,79 @@ export default function Home() {
                 <h1 className="mt-14 text-center text-primary-app-bar text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight">
                     Emma Turinsky
                 </h1>
+            </div>
+            <div className="bg-primary-purple h-screen p-4">
+                <div className="shadow-lg grid grid-cols-1 md:grid-cols-2 gap-2">
+                    <div className="text-center p-2">
+                        <div className="shadow-lg rounded-md bg-secondary-purple text-secondary-app-bar mb-2 py-1">
+                            <h6 className="mb-3 font-bold text-xl">
+                                Personal Information
+                            </h6>
+                            <dl className="mb-2">
+                                <dt className="font-bold">Current Level:</dt>
+                                <dd>3</dd>
+                            </dl>
+                            <dl className="mb-2">
+                                <dt className="font-bold">Gym:</dt>
+                                <dd>Integrity Athletics</dd>
+                            </dl>
+                            <dl className="mb-2">
+                                <dt className="font-bold">Graduation Year:</dt>
+                                <dd>2032</dd>
+                            </dl>
+                            <dl className="mb-2">
+                                <dt className="font-bold">Hometown:</dt>
+                                <dd>Delaware, OH</dd>
+                            </dl>
+                        </div>
+                        <div className="shadow-lg rounded-md bg-secondary-purple text-secondary-app-bar mb-2 py-1">
+                            <h6 className="mb-3 font-bold text-xl">
+                                Achievements
+                            </h6>
+                            <dl className="mb-2">
+                                <dt className="font-bold">Derby Classic:</dt>
+                                <dd>
+                                    1st - Vault, Bars, Floor, AA
+                                </dd>
+                            </dl>
+                            <dl className="mb-2">
+                                <dt className="font-bold">
+                                    Blaine Wilson Sportsfest:
+                                </dt>
+                                <dd>
+                                    1st - Vault, Bars, Floor, AA
+                                </dd>
+                            </dl>
+                            <dl className="mb-2">
+                                <dt className="font-bold">Buckeye Classic:</dt>
+                                <dd>1st - Vault, 2nd - Bars</dd>
+                            </dl>
+                            <dl className="mb-2">
+                                <dt className="font-bold">
+                                    Battle of Champions:
+                                </dt>
+                                <dd>1st - Vault, 2nd - Bars, Floor</dd>
+                            </dl>
+                            <dl className="mb-2">
+                                <dt className="font-bold">Cowtown Classic:</dt>
+                                <dd>1st - Bars</dd>
+                            </dl>
+                            <Link href="/scores" className="underline text-lg hover:decoration-4">
+                                View All Scores
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="p-2">
+                        <Image
+                            src="/emma4-new.jpeg"
+                            alt="Picture of Emma"
+                            width="0"
+                            height="0"
+                            sizes="50vw"
+                            className="w-full h-auto"
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     );
