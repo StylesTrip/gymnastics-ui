52cce62dc76b27d1fa840ffea237e9f3a2b920cb
StylesTrip <ryan.turinsky@gmail.com>
2023-05-06 14:11:29 -0400

    Updates to main page and the score page for better look and feel when on mobile

--- src/app/scores/page.jsx
+++ src/app/scores/page.jsx
@@ -14,10 +14,69 @@ async function getScores() {
 export default async function Page({}) {
     const scores = await getScores();
 
+    const getHighestScoreForEvent = () => {
+        let highScores = {
+            vaultScore: 0,
+            barsScore: 0,
+            beamScore: 0,
+            floorScore: 0,
+            allAroundScore: 0,
+        };
+
+        scores.map((score, key) => {
+            if (score.vault_score >= highScores.vaultScore)
+                highScores.vaultScore = score.vault_score;
+            if (score.bars_score >= highScores.barsScore)
+                highScores.barsScore = score.bars_score;
+            if (score.beam_score >= highScores.beamScore)
+                highScores.beamScore = score.beam_score;
+            if (score.floor_score >= highScores.floorScore)
+                highScores.floorScore = score.floor_score;
+            if (score.all_around_score >= highScores.allAroundScore)
+                highScores.allAroundScore = score.all_around_score;
+        });
+
+        return (
+            <tr className="bg-black border">
+                <td className="border text-center">{highScores.vaultScore}</td>
+                <td className="border text-center">{highScores.barsScore}</td>
+                <td className="border text-center">{highScores.beamScore}</td>
+                <td className="border text-center">{highScores.floorScore}</td>
+                <td className="border text-center">
+                    {highScores.allAroundScore}
+                </td>
+            </tr>
+        );
+    };
+
     return (
-        <div className="w-screen h-screen bg-white">
-            <h1 className="text-3xl font-bold mt-4 underline text-center">Level 3</h1>
-            <section className="flex justify-center mt-4">
+        <div className="w-screen h-screen bg-white p-4">
+            <h1 className="text-3xl font-bold text-center">Level 3</h1>
+            <section className="flex flex-col overflow-x-auto justify-center mt-4">
+                <h2 className="text-2xl font-bold underline text-center">
+                    Personal Best
+                </h2>
+                <table className="table-auto border">
+                    <thead className="border bg-pink-500">
+                        <tr className="border">
+                            <th className="border px-4 pt-4">Vault</th>
+                            <th className="border px-4 pt-4">Uneven Bars</th>
+                            <th className="border px-4 pt-4">Beam</th>
+                            <th className="border px-4 pt-4">Floor</th>
+                            <th className="border px-4 pt-4">All Around</th>
+                        </tr>
+                    </thead>
+                    <tbody className="border">
+                        <Suspense fallback={<tr></tr>}>
+                            {getHighestScoreForEvent()}
+                        </Suspense>
+                    </tbody>
+                </table>
+            </section>
+            <section className="flex flex-col overflow-x-auto justify-center mt-4">
+                <h2 className="text-2xl font-bold underline text-center">
+                    Scores
+                </h2>
                 <table className="table-auto border">
                     <thead className="border bg-pink-500">
                         <tr className="border">
