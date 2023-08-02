1489b19f37bd7f829c8b98eaa70b6513f24c71fd
StylesTrip <ryan.turinsky@gmail.com>
2023-08-01 21:13:23 -0400

    Remove dead code

--- src/app/scores/page.jsx
+++ src/app/scores/page.jsx
@@ -20,41 +20,6 @@ async function getScores() {
 export default async function Page({}) {
     const scores = await getScores();
 
-    const getHighestScoreForEvent = () => {
-        let highScores = {
-            vaultScore: 0,
-            barsScore: 0,
-            beamScore: 0,
-            floorScore: 0,
-            allAroundScore: 0,
-        };
-
-        scores.map((score, key) => {
-            if (score.vault_score >= highScores.vaultScore)
-                highScores.vaultScore = score.vault_score;
-            if (score.bars_score >= highScores.barsScore)
-                highScores.barsScore = score.bars_score;
-            if (score.beam_score >= highScores.beamScore)
-                highScores.beamScore = score.beam_score;
-            if (score.floor_score >= highScores.floorScore)
-                highScores.floorScore = score.floor_score;
-            if (score.all_around_score >= highScores.allAroundScore)
-                highScores.allAroundScore = score.all_around_score;
-        });
-
-        return (
-            <tr className="bg-black border">
-                <td className="border text-center">{highScores.vaultScore}</td>
-                <td className="border text-center">{highScores.barsScore}</td>
-                <td className="border text-center">{highScores.beamScore}</td>
-                <td className="border text-center">{highScores.floorScore}</td>
-                <td className="border text-center">
-                    {highScores.allAroundScore}
-                </td>
-            </tr>
-        );
-    };
-
     return (
         <div className="w-screen h-full md:h-screen bg-white p-4">
             <h1 className="text-3xl font-bold text-center">Level 3</h1>
