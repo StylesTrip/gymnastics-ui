58cf6b65110b254282ec76607e5ae84ec686236a
StylesTrip <ryan.turinsky@gmail.com>
2024-01-23 19:14:07 -0500

    Layout changes for scores page and background color change for videos page

--- src/app/videos/layout.jsx
+++ src/app/videos/layout.jsx
@@ -10,7 +10,7 @@ export default function VideosLayout({
     return (
         <>
             <AppBar variant="filled" />
-            <section className="w-screen h-full bg-white p-4">
+            <section className="w-screen h-full p-4 text-white">
                 {children}
             </section>
         </>
