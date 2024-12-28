9db73847dd18ebf0d4f6c48b261644a90a790a3d
StylesTrip <ryan.turinsky@gmail.com>
2024-12-28 11:16:31 -0500

    Fixed spacing between sections. Fixed getting the proper results per level

--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -30,7 +30,7 @@ export default async function Page({}) {
         <div className="w-full flex flex-col gap-4 md:flex-row p-4 text-white">
             <div className="col-span-5 min-h-screen grow">
                 {levels.reverse().map((level) => (
-                    <>
+                    <div className="mt-4">
                         <h2
                             id={'lvl' + level}
                             className="text-2xl font-bold text-center"
@@ -46,7 +46,8 @@ export default async function Page({}) {
                             </h3>
                             <PersonalBestTable
                                 scores={scores.filter(
-                                    (score) => score.competitions.level === 6
+                                    (score) =>
+                                        score.competitions.level === level
                                 )}
                             />
                         </section>
@@ -59,11 +60,12 @@ export default async function Page({}) {
                             </h3>
                             <ScoresTable
                                 scores={scores.filter(
-                                    (score) => score.competitions.level === 6
+                                    (score) =>
+                                        score.competitions.level === level
                                 )}
                             />
                         </section>
-                    </>
+                    </div>
                 ))}
             </div>
 
