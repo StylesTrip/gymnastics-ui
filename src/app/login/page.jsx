566e5b04ea19f33516d1a44b34724e8d5a8377c4
StylesTrip <ryan.turinsky@gmail.com>
2026-03-01 19:59:33 -0500

    Remove signup button as there are only 2-ish users

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,7 +8,6 @@ export default function LoginPage() {
             <label htmlFor="password">Password:</label>
             <input id="password" name="password" type="password" />
             <button formAction={login}>Log in</button>
-            <button formAction={signup}>Sign up</button>
         </form>
     );
 }
