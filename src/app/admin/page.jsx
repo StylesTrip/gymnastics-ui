dbab68bde60dca23c486b33f3958a07fae7923bc
StylesTrip <ryan.turinsky@gmail.com>
2026-03-01 20:00:09 -0500

    Adding admin and auth page for login. Login done by using email and password to sign in

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,12 @@
+import { createClient } from '@/lib/supabase/server';
+
+export default async function AdminPage() {
+    const supabase = await createClient();
+
+    const { data: { user }, } = await supabase.auth.getUser();
+
+    if (!user) {
+        return <div>Unauthorized</div>;
+    }
+    return <div>Admin Page</div>;
+}
